9923cc49b30d4c41c313bbdfe72751dec3435d5e
mohamed260 <fox2600@yahoo.com>
2021-02-03 14:50:56 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::group(
+	[
+		'prefix' => LaravelLocalization::setLocale(),
+		'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+	], function(){
+
+		Route::get('/', function(){
+			return view('dashboard');
+		});
+});
+
